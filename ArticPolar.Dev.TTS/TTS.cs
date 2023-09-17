178bb197d258129163f7fa6b832416e8770c37ab
ArticPolar <74744598+ArticPolarDEV@users.noreply.github.com>
2023-09-17 20:22:40 +0000

    Source Code

---
+++ ArticPolar.Dev.TTS/TTS.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using System.Speech.Synthesis;
+
+namespace ArticPolar.Dev.TTS
+{
+    public class TTS : IDisposable
+    {
+        private SpeechSynthesizer synthesizer;
+        public TTS()
+        {
+            synthesizer = new SpeechSynthesizer();
+        }
+        public void Speak(string text)
+        {
+            synthesizer.Speak(text);
+        }
+        public void SetVoice(string voiceName)
+        {
+            var voices = synthesizer.GetInstalledVoices();
+            var selectedVoice = voices.FirstOrDefault(v => v.VoiceInfo.Name == voiceName);
+            if (selectedVoice != null)
+            {
+                synthesizer.SelectVoice(selectedVoice.VoiceInfo.Name);
+            }
+        }
+        public void SetRate(int rate) 
+        {
+            synthesizer.Rate = rate;
+        }
+        public void SetVolume(int volume)
+        {
+            synthesizer.Volume = volume;
+        }
+        public void Pause()
+        {
+            synthesizer.Pause();
+        }
+        public void Resume()
+        {
+            synthesizer.Resume();
+        }
+        public void Stop()
+        {
+            synthesizer.SpeakAsyncCancelAll();
+        }
+        public void Dispose()
+        {
+            synthesizer.Dispose();
+        }
+    }
+}
---
+++ TTSConsole/Program.cs
@@ -0,0 +1,28 @@
+﻿using ArticPolar.Dev.TTS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TTSConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var tts = new TTS())
+            {
+                tts.SetVoice("Microsoft Maria Desktop - Portuguese (Brazil)");
+                tts.SetRate(0); // Normal rate
+                tts.SetVolume(100); // Max volume
+
+                tts.Speak("Olá, este é um exemplo de conversão de texto em fala.");
+                tts.Speak("Você pode controlar vários aspectos da síntese de fala com esta classe.");
+
+                Console.WriteLine("Press Enter to exit...");
+                Console.ReadLine();
+            }
+        }
+    }
+}
